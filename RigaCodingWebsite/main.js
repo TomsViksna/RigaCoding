27fc92c35952544df90d8f4b49de7fc32a1fc666
tgspin <Toms>
2018-03-14 00:20:08 +0200

    lol

---
+++ RigaCodingWebsite/main.js
@@ -0,0 +1,175 @@
+var number = 1; //integer - vesels skaitlis
+var decimal = 3.14; //float - decimāls skaitlis
+var text = 'some nice text'; // string - teksts
+var bool = true; //boolian - true or false
+var nothing = null; //nav vērtības
+var cars = ['zaz', 'vaz', 'gaz', 'uvaz', false, 123] //array vai sarakts
+var person = {
+    name: 'John',
+    surename: 'Doe',
+    age: '60'
+}; //objekti
+
+
+var people = [{
+        name: 'John',
+        surename: 'Doe',
+        age: '60'
+    }, //objekti
+
+    {
+        name: 'Tom',
+        surename: 'Doe',
+        age: '60'
+    }, //objekti
+
+    {
+        name: 'Sam',
+        surename: 'Doe',
+        age: '60'
+    }, //objekti
+]
+
+// var name = 'Peteris';
+// console.log(name);
+// name = 'Anniņa';
+// console.log(name);
+
+// var number = 10;
+// var number2 = 2;
+
+// console.log(number / number2);
+// console.log(8 %3);
+
+// console.log("Hellow world");
+// console.log(people);
+
+// var name = 'Jonny';
+//  var othername = 'Anny';
+
+// if (name !== othername) {
+//     console.log('yay');
+// } else {
+//     console.log('boo');
+// }
+
+
+
+// var number = 2;
+
+//     switch (number)
+//     {
+//         case 1: {
+//             console.log("yay1")
+//             break;
+//         }
+
+//         case 2: {
+//             console.log("yay2")
+//             break;
+//         }
+//         case 3: {
+//             console.log("yay3")
+//             break;
+//         }
+//         default: {
+//             console.log("boo")
+//             break;
+//         }
+//     }
+
+
+// var number = 1;
+// var otherNumber = 3;
+// if (number ==1 || otherNumber ==2)
+//     {
+//         console.log('yay');
+//     } else {
+//         console.log('boo');
+//     }
+
+
+//     var number = 1;
+//     if(number == 1)
+//         {
+//             console.log('foo');
+//         }
+
+
+
+
+
+// function calculateVAT(price, vat){
+
+//     var vatNumber = vat / 100 + 1
+//     var priceWithVAT = price * vatNumber
+//     return priceWithVAT;
+// }
+
+// console.log(calculateVAT(3,14, 10));
+
+
+// console.log(typeof parseInt('123'));
+
+
+
+
+
+
+
+// var shouldRun = 0;
+
+// while (shouldRun > 10)
+//     {
+//         console.log('yay, we are in then loop' + shouldRun);
+//         shouldRun = shouldRun + 1;
+//     }
+//         while(shouldRun > 10);
+
+
+// for ( var index = 0; index < cars.length; index++)
+//     {
+//             console.log('yay, we are in the loop' + " " +cars[index]);
+//     }
+
+
+
+
+
+
+// console.log(cars[0]);
+
+
+// for (var index = 0; index < 20; index++) {
+
+//     if(index % 2 == 0) {
+//         console.log(index);
+//     }
+
+// }
+
+//majasdarbs uzrakstīt ciklu, kurā skaitam no 0 līdz 50 un, 
+// ja skaitlis dalās ar 2(bez atlikuma) tad izvadam  "foo", 
+// ja dadlās ar 3 izvadam "bar", 
+// ja dalās ar 5 izvadam "baz", 
+// ja nav neviens no šiem tad izvadam iterācijas numurs(index)
+
+
+for (var index = 0; index < 50; index++) {
+
+    if (index % 2 == 0) {
+        console.log("foo");
+    }
+
+   else if (index % 3 == 0) {
+        console.log("bar");
+    }
+
+   else if (index % 5 == 0) {
+        console.log("baz")
+    }
+
+    else {
+        console.log(index);
+    }
+}
--- RigaCodingWebsite/page2.php
+++ RigaCodingWebsite/page2.php
@@ -1,31 +1,4 @@
-<html lang="en">
 
-<head>
-
-
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-        crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
-        crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-        crossorigin="anonymous"></script>
-
-
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <link rel="stylesheet" type="text/css" href="css/page2.css">
-</head>
 
 <?php include 'header.php';?>
 
@@ -46,7 +19,7 @@
                     <img src="http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg" alt="">
                 </div>
 
-                <div class="col-xs-8">
+                <div class="col-xs-9">
                     <h1>A-fluffy</h1>
                     <p>A-fluffy-cat-looking-funny-surprised-or-concerned A-fluffy-cat-looking-funny-surprised-or-concerned A-fluffy-cat-looking-funny-surprised-or-concerned</p>
                     <p>A-fluffy-cat-looking-funny-surprised-or-concerned A-fluffy-cat-looking-funny-surprised-or-concerned A-fluffy-cat-looking-funny-surprised-or-concerned</p>
